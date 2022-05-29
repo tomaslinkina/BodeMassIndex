a64b48caab80a48c04bc04d4c47a762f7b055510
tomaslinkina <altoma@mail.ru>
2022-05-29 20:33:46 +0500

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,5 @@
+public class BmiService {
+    public double calculate (int mass, double height) {
+        return mass/(height*height);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int mass = 55;
+        double height = 1.65;
+        double index = service.calculate(mass,height);
+        System.out.println(index);
+    }
+}
